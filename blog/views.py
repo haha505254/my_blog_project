8a7a4a23d0ddacd15acd9674e5dc23ea10d9e36d
haha505254 <haha505254@gmail.com>
2022-07-27 18:25:26 +0800

    fixed login url

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-@login_required(login_url="Login")
+
 def index(request):
     return render(request,'blog/index.html')
 
--- my_blog_project/urls.py
+++ my_blog_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index ,name='index'),
     path('blog/',  include('blog.urls')),
+
 ]
