0d5d4c03c6699139082ff63bb5c26aa2fbd0efb4
haha505254 <haha505254@gmail.com>
2022-07-27 22:44:03 +0800

    update blog model

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
-
+    class Meta:
+        unique_together = ('slug', 'author',)
+    
     def __str__(self):
         return self.title
